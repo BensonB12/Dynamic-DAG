01609de1749a0681fc7347af20073c1c4d64f649
Benson Bird <bensonbird2@gmail.com>
2024-11-01 11:12:19 -0600

    Objects

---
+++ graph.py
@@ -0,0 +1,15 @@
+from typing import List
+from pydantic import BaseModel
+from __future__ import annotations
+
+
+class Node(BaseModel):
+    id: int
+    is_s: bool
+    is_t: bool
+    points_to_nodes: List[Node]
+    nodes_that_point_to_me: List[int]
+
+
+class Graph(BaseModel):
+    nodes: List[Node]
