b9a8c087944fbfa1500283d137094a8d46777c75
BensonB12 <bensonbird2@gmail.com>
2024-11-01 12:05:03 -0600

    Testing the models

--- graph.py
+++ graph.py
@@ -1,14 +1,14 @@
+from __future__ import annotations
 from typing import List
 from pydantic import BaseModel
-from __future__ import annotations
 
 
 class Node(BaseModel):
     id: int
-    is_s: bool
-    is_t: bool
-    points_to_nodes: List[Node]
-    node_ids_that_point_to_me: List[int]
+    is_s: bool = False
+    is_t: bool = False
+    points_to_nodes: List[Node] = []
+    node_ids_that_point_to_me: List[int] = []
 
 
 class Graph(BaseModel):
--- test_graph.py
+++ test_graph.py
@@ -1,2 +1,39 @@
+from graph import Node, Graph
+
 def test_pytest():
   assert True
+
+def test_node():
+  n0 = Node(id=0)
+  assert n0
+
+def test_graph():
+  g = Graph(nodes=[])
+  assert g
+
+def test_nodes_go_into_graph():
+  n0 = Node(id=0)
+  g = Graph(nodes=[n0])
+  assert g.nodes[0] == n0
+
+def test_graph_multiple_nodes():
+    n0 = Node(id=0)
+    n1 = Node(id=1)
+    g = Graph(nodes=[n0, n1])
+    assert len(g.nodes) == 2
+    assert g.nodes[0] == n0
+    assert g.nodes[1] == n1
+
+def test_graph_has_references():
+    n1 = Node(id=1, node_ids_that_point_to_me=[0])
+    n0 = Node(id=0, points_to_nodes=[n1])
+    g = Graph(nodes=[n0, n1])
+    assert g.nodes[0].points_to_nodes == [n1]
+    assert g.nodes[1].node_ids_that_point_to_me == [0]
+
+def test_graph_has_t_and_s():
+    n1 = Node(id=1, node_ids_that_point_to_me=[0], is_s=True)
+    n0 = Node(id=0, points_to_nodes=[n1], is_t=True)
+    g = Graph(nodes=[n0, n1])
+    assert g.nodes[0].is_t
+    assert g.nodes[1].is_s
